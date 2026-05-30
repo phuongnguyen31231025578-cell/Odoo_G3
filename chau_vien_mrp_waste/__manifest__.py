14990fb20c74fee867cbe0a3c00b34d17624ee1f
thanhnhungo <thanhngo.31231027074@st.ueh.edu.vn>
2026-05-30 09:33:47 +0700

    Module_Customized_G3_edit1

--- chau_vien_mrp_waste/__manifest__.py
+++ chau_vien_mrp_waste/__manifest__.py
@@ -1,14 +1,17 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Chau Vien MRP Waste Rate",
-    "version": "17.0.1.0.0",
-    "category": "Manufacturing",
-    "summary": "Add waste rate on Bill of Materials for Chau Vien",
-    "author": "ERP Team",
-    "depends": ["mrp"],
-    "data": [
-        "views/mrp_bom_views.xml",
+    'name': 'Chau Vien - MRP Waste Tracking',
+    'version': '19.0.1.0.0',
+    'summary': 'Theo dõi tỷ lệ hao hụt dự kiến và thực tế trong sản xuất bán thành phẩm',
+    'author': 'Chau Vien',
+    'category': 'Manufacturing',
+    'depends': ['mrp'],
+    'data': [
+        'views/mrp_bom_views.xml',
+        'views/mrp_production_views.xml',
     ],
-    "installable": True,
-    "application": False,
-    "license": "LGPL-3",
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
